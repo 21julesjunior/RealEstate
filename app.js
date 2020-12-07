003fd408ef3e8212991c834f7d8447316c8ac1e9
21julesjunior <75339052+21julesjunior@users.noreply.github.com>
2020-12-07 23:16:27 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const sr = ScrollReveal({
+    duration: 1000,
+    reset: true
+});
+
+sr.reveal('h1',  {
+     
+});
+
+sr.reveal('h2',  {
+    
+});
+
+sr.reveal('p',  {
+    delay: 200
+});
+
+sr.reveal('',  {
+    delay: 200
+});
+
+sr.reveal('',  {
+    delay: 200
+});
+
+sr.reveal('iframe',  {
+    delay: 200
+});
+
+sr.reveal('.btn',  {
+    delay: 200
+});
+
+
+
+
+
+ 
